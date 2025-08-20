9991668f9e5fb2f17c8658fbf215a527d964cd9a
Roshankhan6786 <useranroid0@gmail.com>
2025-08-20 09:55:40 +0530

    created the rough structure of the contract

--- src/GasPriceFeesHook.sol
+++ src/GasPriceFeesHook.sol
@@ -1,4 +1,84 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import {BaseHook} from "
+import {BaseHook} from "v4-periphery/src/utils/BaseHook.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
+import {LPFeeLibrary} from "v4-core/libraries/LPFeeLibrary.sol";
+import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/types/BeforeSwapDelta.sol";
+import {SwapParams} from "v4-core/types/PoolOperation.sol";
+
+contract GasPriceFeesHook is BaseHook {
+    using LPFeeLibrary for uint24;
+
+    //var to keep track of moving average gas price
+    uint128 public movingAverageGasPrice;
+
+    //var to keep track of how many times the moving average has been updated
+    //needed as denominator in the formula
+    uint104 public movingAverageGasPriceCount;
+
+    //the base fees for the pool
+    uint24 public constant BASE_FEE = 5000;
+
+    error MustUseDynamicFee();
+
+    constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
+
+    //override funciton to let the PoolManager know which things are enabled for this hook
+    function getHookPermissions()
+        public
+        pure
+        override
+        returns (Hooks.Permissions memory)
+    {
+        return
+            Hooks.Permissions({
+                beforeInitialize: true,
+                afterInitialize: false,
+                beforeAddLiquidity: false,
+                beforeRemoveLiquidity: false,
+                afterAddLiquidity: false,
+                afterRemoveLiquidity: false,
+                beforeSwap: true,
+                afterSwap: true,
+                beforeDonate: false,
+                afterDonate: false,
+                beforeSwapReturnDelta: false,
+                afterSwapReturnDelta: false,
+                afterAddLiquidityReturnDelta: false,
+                afterRemoveLiquidityReturnDelta: false
+            });
+    }
+
+    function _beforeInitialize(
+        address,
+        PoolKey calldata key,
+        uint160
+    ) internal pure override returns (bytes4) {
+        return this.beforeInitialize.selector;
+    }
+
+    function _beforeSwap(
+        address,
+        PoolKey calldata key,
+        SwapParams calldata,
+        bytes calldata
+    ) internal view override returns (bytes4, BeforeSwapDelta, uint24) {
+        // TODO
+        return (this.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, 0);
+    }
+
+    function _afterSwap(
+        address,
+        PoolKey calldata,
+        SwapParams calldata,
+        BalanceDelta,
+        bytes calldata
+    ) internal override returns (bytes4, int128) {
+        // TODO
+        return (this.afterSwap.selector, 0);
+    }
+}
